96563b8c3acca7ef42dadf9e4c48176560cae8cd
greg higgins <greg.higgins@v12technology.com>
2021-12-29 22:25:38 +0000

    adding gui representation of the running services

---
+++ src/main/java/com/fluxtion/example/servicestarter/example1/GuiMain.java
@@ -0,0 +1,208 @@
+package com.fluxtion.example.servicestarter.example1;
+
+import com.fluxtion.example.servicestater.Service;
+import com.fluxtion.example.servicestater.ServiceManager;
+import com.fluxtion.example.servicestater.ServiceStatusRecord;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.fluxtion.example.servicestarter.example1.Main.*;
+
+public class GuiMain {
+    private JTable statusTable;
+    private final List<ServiceStatusRecord> statusUpdate = new ArrayList<>();
+    private final Map<String, JLabel> nodeMap = new HashMap<>();
+    private DefaultTableModel tableModel;
+    private JPanel buttonPanel;
+    private final ServiceManager serviceManager;
+    private JPanel graphPanel;
+
+    public GuiMain(ServiceManager serviceManager) {
+        this.serviceManager = serviceManager;
+        JFrame mainFrame = new JFrame("Service starter example");
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setLayout(new BorderLayout());
+        buildGraphPanel();
+        buildTableModel();
+        buildButtonBar();
+        mainFrame.add(new JScrollPane(statusTable), BorderLayout.NORTH);
+        mainFrame.add(graphPanel, BorderLayout.CENTER);
+        mainFrame.add(buttonPanel, BorderLayout.SOUTH);
+        mainFrame.setPreferredSize (new Dimension (900, 650));
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+
+    private void buildGraphPanel() {
+        graphPanel = new JPanel();
+        graphPanel.setBackground(new Color(225, 225, 225));
+        graphPanel.setLayout(null);
+        graphPanel.setPreferredSize(new Dimension(800, 500));
+        graphPanel.setMinimumSize(new Dimension(800, 500));
+        addNode(ORDER_GATEWAY, 325, 50);
+        addNode(PNL_CHECK, 325, 150);
+        addNode(LIMIT_READER, 50, 150);
+        addNode(MARKET_DATA_GATEWAY, 600, 150);
+    }
+
+    private void addNode(String name, int horizontal, int vertical){
+        JLabel node = new JLabel(name);
+        node.setBackground(Color.lightGray);
+        node.setForeground(Color.BLUE);
+        node.setBorder(new LineBorder(Color.BLACK, 1));
+        node.setHorizontalAlignment(SwingConstants.CENTER);
+        node.setOpaque(true);
+        final int width = 200;
+        final int height = 45;
+        graphPanel.add(node);
+        node.setBounds(horizontal, vertical, width, height);
+        nodeMap.put(name, node);
+    }
+
+    private void buildButtonBar() {
+        buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.WHITE);
+        buttonPanel.setLayout(new FlowLayout());
+        JButton start_all = new JButton("start all");
+        JButton stop_all = new JButton("stop all");
+        start_all.addActionListener(e -> serviceManager.startAllServices());
+        stop_all.addActionListener(e -> serviceManager.stopAllServices());
+
+        JCheckBox triggerOnTaskComplete = new JCheckBox("trigger on task complete", true);
+        triggerOnTaskComplete.addItemListener(i ->
+                serviceManager.triggerNotificationOnSuccessfulTaskExecution(i.getStateChange() == ItemEvent.SELECTED)
+        );
+
+        JCheckBox triggerOnNotification = new JCheckBox("trigger on status update", true);
+        triggerOnNotification.addItemListener(i ->
+                serviceManager.triggerDependentsOnNotification(i.getStateChange() == ItemEvent.SELECTED)
+        );
+        buttonPanel.add(start_all);
+        buttonPanel.add(stop_all);
+        buttonPanel.add(triggerOnTaskComplete);
+        buttonPanel.add(triggerOnNotification);
+    }
+
+    public void logStatus(List<ServiceStatusRecord> statusUpdate) {
+        SwingUtilities.invokeLater(() -> {
+            this.statusUpdate.clear();
+            this.statusUpdate.addAll(statusUpdate);
+            tableModel.fireTableDataChanged();
+        });
+    }
+
+    private void buildTableModel() {
+        tableModel = new DefaultTableModel() {
+            @Override
+            public int getColumnCount() {
+                return 6;
+            }
+
+            @Override
+            public int getRowCount() {
+                return statusUpdate.size();
+            }
+
+            @Override
+            public Object getValueAt(int row, int column) {
+                ServiceStatusRecord serviceStatusRecord = statusUpdate.get(row);
+                return switch (column) {
+                    case 0 -> serviceStatusRecord.getServiceName();
+                    case 1 -> serviceStatusRecord.getStatus();
+                    case 2 -> "start service";
+                    case 3 -> "stop service";
+                    case 4 -> "notify started";
+                    case 5 -> "notify stopped";
+                    default -> throw new IllegalStateException("Unexpected value: " + column);
+                };
+            }
+
+            @Override
+            public String getColumnName(int column) {
+                return switch (column) {
+                    case 0 -> "Service name";
+                    case 1 -> "Status";
+                    case 2 -> "Start service";
+                    case 3 -> "Stop service";
+                    case 4 -> "Notify started";
+                    case 5 -> "Notify stopped";
+                    default -> throw new IllegalStateException("Unexpected value: " + column);
+                };
+            }
+
+            @Override
+            public void setValueAt(Object aValue, int row, int column) {
+                String svcName = statusUpdate.get(row).getServiceName();
+                System.out.println(aValue + "(" + svcName + ")");
+                switch ((String)aValue){
+                    case "start service" -> serviceManager.startService(svcName);
+                    case "stop service" -> serviceManager.stopService(svcName);
+                    case "notify started" -> serviceManager.serviceStarted(svcName);
+                    case "notify stopped" -> serviceManager.serviceStopped(svcName);
+                }
+            }
+
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return column > 1;
+            }
+        };
+        statusTable = new JTable(tableModel);
+        statusTable.setPreferredSize(new Dimension(800, 150));
+        statusTable.setPreferredScrollableViewportSize(new Dimension(800, 150));
+        statusTable.setRowSelectionAllowed(false);
+        statusTable.setColumnSelectionAllowed(false);
+        TableColumnModel columnModel = statusTable.getColumnModel();
+        columnModel.getColumn(0).setMinWidth(150);
+        columnModel.getColumn(0).setMaxWidth(300);
+        columnModel.getColumn(1).setMinWidth(130);
+        columnModel.getColumn(1).setCellRenderer(new MyRenderer(Color.lightGray, Color.blue));
+        new ButtonColumn(statusTable,  2);
+        new ButtonColumn(statusTable,  3);
+        new ButtonColumn(statusTable,  4);
+        new ButtonColumn(statusTable,  5);
+    }
+
+    // Customize the code to set the color for each column in JTable
+    class MyRenderer extends DefaultTableCellRenderer
+    {
+        Color bg, fg;
+        public MyRenderer(Color bg, Color fg) {
+            super();
+            this.bg = bg;
+            this.fg = fg;
+        }
+        public Component getTableCellRendererComponent(JTable table, Object
+                value, boolean isSelected, boolean hasFocus, int row, int column)
+        {
+            Component cell = super.getTableCellRendererComponent(table, value,
+                    isSelected, hasFocus, row, column);
+            ServiceStatusRecord serviceStatusRecord = statusUpdate.get(row);
+            JLabel jComponent = nodeMap.get(serviceStatusRecord.getServiceName());
+            jComponent.setText("<html>" + serviceStatusRecord.getServiceName() + "<br>" + serviceStatusRecord.getStatus() + "</html>");
+            if(serviceStatusRecord.getStatus()== Service.Status.STARTED){
+                Color green     = new Color(20, 170, 40);
+                cell.setForeground(green);
+                jComponent.setForeground(green);
+            }else if(serviceStatusRecord.getStatus()== Service.Status.STOPPED){
+                cell.setForeground(Color.RED);
+                jComponent.setForeground(Color.RED);
+            }else{
+                cell.setForeground(fg);
+                jComponent.setForeground(fg);
+            }
+            cell.setBackground(bg);
+            return cell;
+        }
+    }
+}
--- src/main/java/com/fluxtion/example/servicestarter/example1/Main.java
+++ src/main/java/com/fluxtion/example/servicestarter/example1/Main.java
@@ -3,6 +3,8 @@ package com.fluxtion.example.servicestarter.example1;
 import com.fluxtion.example.servicestater.Service;
 import com.fluxtion.example.servicestater.ServiceManager;
 import com.fluxtion.example.servicestater.ServiceStatusRecord;
+import com.fluxtion.example.servicestater.helpers.AsynchronousTaskExecutor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -12,38 +14,42 @@ import java.util.stream.Collectors;
 @Slf4j
 public class Main {
 
+    public static final String ORDER_GATEWAY = "orderGateway";
+    public static final String LIMIT_READER = "limitReader";
+    public static final String MARKET_DATA_GATEWAY = "marketDataGateway";
+    public static final String PNL_CHECK = "pnlCheck";
+
     public static void main(String[] args) {
-        Service orderGateway = Service.builder("orderGateway")
+        Service orderGateway = Service.builder(ORDER_GATEWAY)
                 .startTask(Main::emptyTask)
+                .stopTask(Main::emptyTask)
                 .build();
-        Service limitReader = Service.builder("limitReader")
+        Service limitReader = Service.builder(LIMIT_READER)
                 .startTask(Main::emptyTask)
+                .stopTask(Main::emptyTask)
                 .build();
-        Service marketDataGateway = Service.builder("marketDataGateway")
+        Service marketDataGateway = Service.builder(MARKET_DATA_GATEWAY)
                 .startTask(Main::emptyTask)
+                .stopTask(Main::emptyTask)
                 .build();
-        Service pnlCheck = Service.builder("pnlCheck")
+        Service pnlCheck = Service.builder(PNL_CHECK)
                 .requiredServices(limitReader, marketDataGateway)
                 .servicesThatRequireMe(orderGateway)
+                .stopTask(Main::emptyTask)
                 .startTask(Main::emptyTask)
                 .build();
 
 
         ServiceManager svcManager = ServiceManager.build(orderGateway, limitReader, marketDataGateway, pnlCheck);
+        svcManager.registerTaskExecutor(new AsynchronousTaskExecutor());
         svcManager.triggerDependentsOnNotification(true);
         svcManager.triggerNotificationOnSuccessfulTaskExecution(true);
-        svcManager.registerStatusListener(Main::logStatus);
-        svcManager.startAllServices();
+        svcManager.registerStatusListener(new GuiMain(svcManager)::logStatus);
     }
 
-    public static void logStatus(List<ServiceStatusRecord> statusUpdate){
-        log.info("Current status:\n" +
-                statusUpdate.stream()
-                        .map(Objects::toString)
-                        .collect(Collectors.joining("\n"))
-        );
-    }
 
-    public static void emptyTask(){
+    @SneakyThrows
+    public static void emptyTask() {
+        Thread.sleep(1_500);
     }
 }
